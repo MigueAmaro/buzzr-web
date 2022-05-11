cdd697e9177a9d629decfcdb2fef0f816977eb42
Jose Velasquez <velasquezdenorajose@gmail.com>
2022-05-10 21:37:27 -0400

    [UPDATE] Repository update without useless components

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Trabajando en Buzzr App, vuelva mas tarde</h1>
-	</div>
-);
+export const Home = () => {
+	return(
+		<div className="text-center mt-5">
+			<h1>Trabajando en Buzzr App, vuelva mas tarde</h1>
+		</div>
+	)
+};
