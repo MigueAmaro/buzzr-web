ecf1783f2050db3a828d92d6625745eaf19ac872
Jose Velasquez <velasquezdenorajose@gmail.com>
2022-05-12 18:40:11 -0400

    [FIX] Logout store

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -3,8 +3,8 @@ const getState = ({ getStore, getActions, setStore }) => {
 		store: {
 			token: localStorage.getItem("token") || "",
 			// Cambiar la urlBase segun se necesite
-			urlBase: "http://127.0.0.1:5000"
-			// urlBase: "https://5000-migueamaro-buzzrapi-i42rbl8mdfv.ws-us44.gitpod.io"
+			// urlBase: "http://127.0.0.1:5000"
+			urlBase: "https://5000-migueamaro-buzzrapi-0zwwoy345m7.ws-us44.gitpod.io"
 		},
 		actions: {
 			handleLogin: async (login) => {
@@ -70,6 +70,11 @@ const getState = ({ getStore, getActions, setStore }) => {
 				}
 			},
 			handleLogout: () =>{
+				let store = getStore();
+				setStore({
+					...store,
+					token: ""
+				})
 				localStorage.removeItem("token")
 			}
 			}
