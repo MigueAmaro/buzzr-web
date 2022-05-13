acd639d5707ac7ad14a83d7d33c09ca3cc4f06e9
Jose Morrone <josemmorrone@gmail.com>
2022-05-13 19:11:15 -0400

    [UPGRADE] show the user info in the screen

--- src/js/component/UserInfo.jsx
+++ src/js/component/UserInfo.jsx
@@ -1,41 +1,72 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Context } from "../store/appContext.js";
 
 const UserInfo = () => {
     const { store, actions } = useContext(Context)
-    let info = {}
-    const handleUser = async () =>{
-        try{
-            let response = await fetch(`${store.urlBase}/user/${store.id}`,{
+
+    const [info, setinfo] = useState({})
+
+    const handleUser = async () => {
+        try {
+            let response = await fetch(`${store.urlBase}/user/${store.id}`, {
                 method: 'GET',
-                headers:{
-                    "Content-Type":"application/json",
+                headers: {
+                    "Content-Type": "application/json",
                     "Authorization": `Bearer ${store.token}`
                 }
             })
-            if(response.ok){
+            if (response.ok) {
                 let data = await response.json()
-                info = data
-                return info
+                setinfo(data)
             }
-            else{
+            else {
                 console.log(response.json())
             }
         }
-        catch(error){
+        catch (error) {
             console.log(error)
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         handleUser()
-    },[])
+    }, [])
 
 
     return (
-        <div>
-            <p> Hola {info?.username}</p>
-        </div>
+        <>
+            <form>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp"
+                    value={info.email}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="password"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Name</label>
+                    <input type="email" className="form-control" id="name" aria-describedby="emailHelp"
+                    value={info.first_name}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Last Name</label>
+                    <input type="email" className="form-control" id="last_name" aria-describedby="emailHelp"
+                    value={info.last_name}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Username</label>
+                    <input type="email" className="form-control" id="username" aria-describedby="emailHelp"
+                    value={info.username}
+                    />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </>
     );
 };
 
