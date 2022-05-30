75350ca70fb03a91fbf2cd22138ee272f320e525
Jose Morrone <josemmorrone@gmail.com>
2022-05-30 14:51:14 -0400

    send the msg to the back

--- src/js/component/channelChat.jsx
+++ src/js/component/channelChat.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { useState } from 'react';
+import io from "socket.io-client";
+import { useParams } from "react-router";
+import { useEffect } from 'react';
+
+
+const endPoint = process.env.ENDPOINT;
+let socket = io.connect(`${endPoint}`);
 
 const ChannelChat = () => {
+
+    const [message, setMessage] = useState("")
+	let params = useParams()
+
+    const handleKeyDown = (event) => {
+        if (event.key == "Enter") {
+			socket.emit("channel_chat", {"msg": message, "channel": params.name});
+			setMessage("");
+		}
+    }
+
+    socket.on("get_channel_chat", (msg) =>{
+        console.log(msg)
+    })
+
     return (
         <div>
-            <h1>Hola</h1>
+            <input 
+            value={message}
+            name="msg"
+            onChange={(e) => {setMessage(e.target.value)}}
+            onKeyDown={(e) => {handleKeyDown(e)}}
+            ></input>
         </div>
     );
 };
