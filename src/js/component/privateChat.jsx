60ebb7d6be71c31981628a0951618d8175bfb437
Jose Morrone <josemmorrone@gmail.com>
2022-06-02 17:51:29 -0400

    adding style to the private chat

--- src/js/component/privateChat.jsx
+++ src/js/component/privateChat.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect, useContext } from "react";
 import io from "socket.io-client";
 import { Context } from "../store/appContext"
+import { Helmet } from "react-helmet"
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+
+
 
 const endPoint = process.env.ENDPOINT;
 const id = localStorage.getItem("id")
@@ -8,41 +13,36 @@ let privateSocket = io.connect(`${endPoint}`);
 
 const PrivateChat = () => {
     const { store, actions } = useContext(Context)
+    let params = useParams()
 
     const [messages, setMessages] = useState(["Hello And Welcome"]);
     const [message, setMessage] = useState("");
-    const [username, setUsername] = useState('')
 
 
     const getMessages = () => {
+        actions.handlePrivateMessages(params.id)
         privateSocket.on("new_private_msg", (msg) => {
-            console.log("nothing to see here")
             setMessages([...messages, msg]);
         });
     };
 
     // On Change
     const onChange = (e) => {
-        if (e.target.name == "username") {
-            setUsername(e.target.value)
-        } else {
-            setMessage(e.target.value);
-        }
+        setMessage(e.target.value);
     };
 
     // On Enter
     const handleKeyDown = (event) => {
         if (event.key == "Enter") {
-            privateSocket.emit("private_message", { 'username': username, 'id': id, 'msg': message });
+            privateSocket.emit("private_message", { 'username': params.username, 'id': id, 'msg': message });
             setMessage("");
-            setUsername('');
         }
     };
 
     // On Click
     const onClick = () => {
         if (message !== "") {
-            socket.emit("message", message);
+            privateSocket.emit("private_message", { 'username': params.username, 'id': id, 'msg': message });
             setMessage("");
         } else {
             alert("Please Add A Message");
@@ -51,58 +51,61 @@ const PrivateChat = () => {
 
     useEffect(() => {
         getMessages();
-        actions.handleAllUsers()
         privateSocket.emit("login", id)
     }, [messages]);
 
 
     return (
         <>
-            <div className="d-flex">
-
-                {store.allUsers.length > 0 ? (
+            <div className='row d-flex justify-content'>
+                <Helmet>
+                    <style>{'body {background-color: rgba(33,37,41);}'}</style>
+                </Helmet>
+                <div className='channel_title'>
+                    <h2>{params.username}</h2>
+                </div>
+                <div className="col-2 channels_and_users"></div>
+                <div className='bg-dark col-8 chat_body'>
                     <ul>
-                        <li>Usuarios en el chat:</li>
-                        {store.allUsers.map((user) => {
-                            return (
-                                <li
-                                    key={user.id}
-                                    onClick={() => actions.handlePrivateMessages(user.id)}>{user.username}</li>
-                            )
-                        })}
+                        <li>Titulo del chat: {params.name}</li>
+                        {store.messages.length > 0 &&
+                            store.messages.map((msg) => {
+                                return (
+                                    <div>
+                                        {store.userInfo.username == msg.username_from ?
+                                            <li key={msg.id} className="my_messages">
+                                                <div className='d-flex justify-content-end'>{msg.username_from}</div>
+                                                <div className='row'><div className='col-2'>{msg.date}</div> <div className='d-flex justify-content-center align-self-center col-10'>{msg.msg}</div></div>
+                                            </li>
+                                            :
+                                            <li key={msg.id} className="other_messages">
+                                                <div className='d-flex justify-content-start'>{msg.username_from}</div>
+                                                <div className='row'> <div className='d-flex justify-content-center align-self-center col-10'>{msg.msg}</div><div className='col-2'>{msg.date}</div></div>
+                                            </li>
+                                        }
+                                    </div>
+                                )
+                            })}
                     </ul>
-                ) : (
-                    <div>No hay usuarios en este chat</div>
-                )}
-                <ul>
-                    {store.messages.length > 0 &&
-                        store.messages.map((msg) => {
-                            return(
-                            <li key={msg.id}>
-                                {msg.username_from}: {msg.msg}, {msg.date}
-                            </li>
-                            )
-                        }
-
-                        )}
-                </ul>
+                </div>
+                <div className="col-2 channels_and_users"></div>
             </div>
-            <p>Username</p>
-            <input
-                className="me-2"
-                value={username}
-                name="username"
-                onChange={(e) => onChange(e)}
-                onKeyDown={(event) => handleKeyDown(event)}
-            />
-            <p className="mt-2">Message</p>
-            <input
-                value={message}
-                name="message"
-                onChange={(e) => onChange(e)}
-                onKeyDown={(event) => handleKeyDown(event)}
-            />
-            <button onClick={() => onClick()}>Send Message</button>
+            <div className='row'>
+                <div className="col-2"></div>
+                <div className='col-8 d-flex justify-content-center align-items-end'>
+                    <input
+                        value={message}
+                        name="message"
+                        onChange={(e) => { setMessage(e.target.value) }}
+                        onKeyDown={(e) => { handleKeyDown(e) }}
+                        type="text" className='chat_input col-10 mt-4'
+                    ></input>
+                    <button type='button' className='send_button btn btn-outline-primary' onClick={() => {
+                        onClick()
+                    }}><i className="far fa-paper-plane"></i></button>
+                </div>
+                <div className="col-2"></div>
+            </div>
         </>
     );
 };
